02dec128c4e77fafc9c436f9cc31134a93b3d05a
atesti <andres.a.testi@gmail.com>
2014-09-05 10:55:40 -0300

    more changes

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -4,48 +4,39 @@
  * Provides methods for the Hello Endpoints sample UI and interaction with the
  * Hello Endpoints API.
  *
- * @author danielholevoet@google.com (Dan Holevoet)
+ * @author Andrés Testi
  */
 
-/** google global namespace for Google projects. */
-var google = google || {};
-
-/** devrel namespace for Google Developer Relations projects. */
-google.devrel = google.devrel || {};
-
-/** samples namespace for DevRel sample code. */
-google.devrel.samples = google.devrel.samples || {};
-
-/** hello namespace for this sample. */
-google.devrel.samples.hello = google.devrel.samples.hello || {};
+/** blobstoretest namespace. */
+var blobstoretest = blobstoretest || {};
 
 /**
  * Client ID of the application (from the APIs Console).
  * @type {string}
  */
-google.devrel.samples.hello.CLIENT_ID =
-    'replace this with your web client ID';
+blobstoretest.CLIENT_ID =
+    '370378822481-70j2omuds3g3gfqhualkourlmkha1jun.apps.googleusercontent.com';
 
 /**
  * Scopes used by the application.
  * @type {string}
  */
-google.devrel.samples.hello.SCOPES =
+blobstoretest.SCOPES =
     'https://www.googleapis.com/auth/userinfo.email';
 
 /**
  * Whether or not the user is signed in.
  * @type {boolean}
  */
-google.devrel.samples.hello.signedIn = false;
+blobstoretest.signedIn = false;
 
 /**
  * Loads the application UI after the user has completed auth.
  */
-google.devrel.samples.hello.userAuthed = function() {
+blobstoretest.userAuthed = function() {
   var request = gapi.client.oauth2.userinfo.get().execute(function(resp) {
     if (!resp.code) {
-      google.devrel.samples.hello.signedIn = true;
+    	blobstoretest.signedIn = true;
       document.getElementById('signinButton').innerHTML = 'Sign out';
       document.getElementById('authedGreeting').disabled = false;
     }
@@ -57,34 +48,34 @@ google.devrel.samples.hello.userAuthed = function() {
  * @param {boolean} mode Whether or not to use immediate mode.
  * @param {Function} callback Callback to call on completion.
  */
-google.devrel.samples.hello.signin = function(mode, callback) {
-  gapi.auth.authorize({client_id: google.devrel.samples.hello.CLIENT_ID,
-      scope: google.devrel.samples.hello.SCOPES, immediate: mode},
+blobstoretest.signin = function(mode, callback) {
+  gapi.auth.authorize({client_id: blobstoretest.CLIENT_ID,
+      scope: blobstoretest.SCOPES, immediate: mode},
       callback);
 };
 
 /**
  * Presents the user with the authorization popup.
  */
-google.devrel.samples.hello.auth = function() {
-  if (!google.devrel.samples.hello.signedIn) {
-    google.devrel.samples.hello.signin(false,
-        google.devrel.samples.hello.userAuthed);
+blobstoretest.auth = function() {
+  if (!blobstoretest.signedIn) {
+    blobstoretest.signin(false,
+        blobstoretest.userAuthed);
   } else {
-    google.devrel.samples.hello.signedIn = false;
+    blobstoretest.signedIn = false;
     document.getElementById('signinButton').innerHTML = 'Sign in';
     document.getElementById('authedGreeting').disabled = true;
   }
 };
 
 /**
- * Prints a greeting to the greeting log.
+ * Prints a fileData to the greeting log.
  * param {Object} greeting Greeting to print.
  */
-google.devrel.samples.hello.print = function(greeting) {
+blobstoretest.print = function(fileData) {
   var element = document.createElement('div');
   element.classList.add('row');
-  element.innerHTML = greeting.message;
+  element.innerHTML = fileData.filename;
   document.getElementById('outputLog').appendChild(element);
 };
 
@@ -92,8 +83,8 @@ google.devrel.samples.hello.print = function(greeting) {
  * Gets a numbered greeting via the API.
  * @param {string} id ID of the greeting.
  */
-google.devrel.samples.hello.getGreeting = function(id) {
-  gapi.client.helloworld.greetings.getGreeting({'id': id}).execute(
+blobstoretest.getGreeting = function(id) {
+  gapi.client.blobstoretest.blobstoretest.getGreeting({'id': id}).execute(
       function(resp) {
         if (!resp.code) {
           google.devrel.samples.hello.print(resp);
@@ -106,8 +97,8 @@ google.devrel.samples.hello.getGreeting = function(id) {
 /**
  * Lists greetings via the API.
  */
-google.devrel.samples.hello.listGreeting = function() {
-  gapi.client.helloworld.greetings.listGreeting().execute(
+blobstoretest.listGreeting = function() {
+  gapi.client.blobstoretest.blobstoretest.listGreeting().execute(
       function(resp) {
         if (!resp.code) {
           resp.items = resp.items || [];
@@ -123,9 +114,9 @@ google.devrel.samples.hello.listGreeting = function() {
  * @param {string} greeting Greeting to repeat.
  * @param {string} count Number of times to repeat it.
  */
-google.devrel.samples.hello.multiplyGreeting = function(
+blobstoretest.multiplyGreeting = function(
     greeting, times) {
-  gapi.client.helloworld.greetings.multiply({
+  gapi.client.blobstoretest.blobstoretest.multiply({
       'message': greeting,
       'times': times
     }).execute(function(resp) {
@@ -138,38 +129,38 @@ google.devrel.samples.hello.multiplyGreeting = function(
 /**
  * Greets the current user via the API.
  */
-google.devrel.samples.hello.authedGreeting = function(id) {
-  gapi.client.helloworld.greetings.authed().execute(
+blobstoretest.authedGreeting = function(id) {
+	blobstoretest.authed().execute(
       function(resp) {
-        google.devrel.samples.hello.print(resp);
+    	  blobstoretest.print(resp);
       });
 };
 
 /**
  * Enables the button callbacks in the UI.
  */
-google.devrel.samples.hello.enableButtons = function() {
+blobstoretest.enableButtons = function() {
   document.getElementById('getGreeting').onclick = function() {
-    google.devrel.samples.hello.getGreeting(
+	  blobstoretest.getGreeting(
         document.getElementById('id').value);
   }
 
   document.getElementById('listGreeting').onclick = function() {
-    google.devrel.samples.hello.listGreeting();
+	  blobstoretest.listGreeting();
   }
 
   document.getElementById('multiplyGreetings').onclick = function() {
-    google.devrel.samples.hello.multiplyGreeting(
+	  blobstoretest.multiplyGreeting(
         document.getElementById('greeting').value,
         document.getElementById('count').value);
   }
 
   document.getElementById('authedGreeting').onclick = function() {
-    google.devrel.samples.hello.authedGreeting();
+	  blobstoretest.authedGreeting();
   }
   
   document.getElementById('signinButton').onclick = function() {
-    google.devrel.samples.hello.auth();
+	  blobstoretest.auth();
   }
 };
 
@@ -177,19 +168,19 @@ google.devrel.samples.hello.enableButtons = function() {
  * Initializes the application.
  * @param {string} apiRoot Root of the API's path.
  */
-google.devrel.samples.hello.init = function(apiRoot) {
+blobstoretest.init = function(apiRoot) {
   // Loads the OAuth and helloworld APIs asynchronously, and triggers login
   // when they have completed.
   var apisToLoad;
   var callback = function() {
     if (--apisToLoad == 0) {
-      google.devrel.samples.hello.enableButtons();
-      google.devrel.samples.hello.signin(true,
-          google.devrel.samples.hello.userAuthed);
+    	blobstoretest.enableButtons();
+    	blobstoretest.signin(true,
+    			blobstoretest.userAuthed);
     }
   }
 
   apisToLoad = 2; // must match number of calls to gapi.client.load()
-  gapi.client.load('helloworld', 'v1', callback, apiRoot);
+  gapi.client.load('blobstoretest', 'v1', callback, apiRoot);
   gapi.client.load('oauth2', 'v2', callback);
 };
